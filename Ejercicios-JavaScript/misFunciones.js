dbda26ab00cea0a3f182a12d334af71d15fbea4a
amireujanian <1901643@ucc.edu.ar>
2020-05-09 19:59:20 -0300

    Java Script primera web, funciones y conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,31 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function conversor(id, valor){
+    if(isNaN(valor)){
+        alert('Se ingreso un valor invalido');
+        document.unidades.unid_metro.value="";
+        document.unidades.unid_pulgada.value = "";
+        document.unidades.unid_pie.value = "";
+        document.unidades.unid_yarda.value = "";
+    }else if(id=="metro"){
+        document.unidades.unid_pulgada.value = 39.3701*valor;
+        document.unidades.unid_pie.value = 3.28084*valor;
+        document.unidades.unid_yarda.value = 1.09361*valor;
+    }else if(id=="yarda"){
+        document.unidades.unid_pulgada.value = 36*valor;
+        document.unidades.unid_pie.value = 3*valor;
+        document.unidades.unid_metro.value = 0.9144*valor;
+    }else if(id=="pie"){
+        document.unidades.unid_pulgada.value = 12*valor;
+        document.unidades.unid_metro.value = 0.3048*valor;
+        document.unidades.unid_yarda.value = 0.333333*valor;
+    }else if(id=="pulgada"){
+        document.unidades.unid_pie.value = 0.0833333*valor;
+        document.unidades.unid_metro.value = 0.0254*valor;
+        document.unidades.unid_yarda.value = 0.0277778*valor;
+    }
+}
+
+
