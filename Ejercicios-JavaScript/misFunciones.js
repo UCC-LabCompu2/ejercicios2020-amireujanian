0e699e70017a291098efd00d9f7d59cb136be702
amireujanian <1901643@ucc.edu.ar>
2020-06-26 02:07:24 -0300

    Se realizo ejercicio agregar imagen a un Canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -240,3 +240,19 @@ function dibujarCuad() {
     ctx.stroke();
     ctx.closePath();
 }
+
+function dibujarImagen(pX, pY) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    console.log(pX,pY);
+
+    var img = new Image();
+    img.src = "images/auto.png";
+
+    canvas.width = canvas.width;
+
+    img.onload = function(){
+        ctx.drawImage(img, pX,pY);
+    }
+
+}
